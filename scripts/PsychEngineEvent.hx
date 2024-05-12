62fb46b8c55e576666ed7c4154c855508e2c03eb
Junior <87715171+JuniorNovoa1@users.noreply.github.com>
2024-05-12 12:41:05 -0400

    fixes, and a SINGLE readme

--- scripts/PsychEngineEvent.hx
+++ scripts/PsychEngineEvent.hx
@@ -1,62 +1,44 @@
-import flixel.FlxSprite;
+import funkin.play.character.CharacterType;
+import funkin.modding.module.ModuleHandler;
+import funkin.modding.module.Module;
 import funkin.play.character.BaseCharacter;
-
-import funkin.play.event.ScriptedSongEvent;
-// Data from the chart
-import funkin.data.song.SongData;
-import funkin.data.song.SongData.SongEventData;
-// Data from the event schema
+import funkin.play.character.CharacterDataParser;
 import funkin.play.event.SongEvent;
-import funkin.data.event.SongEventSchema;
-import funkin.data.event.SongEventSchema.SongEventFieldType;
+import funkin.play.event.ScriptedSongEvent;
+import funkin.play.PlayState;
 
 class PsychEngineEvent extends ScriptedSongEvent
 {
-  public function new()
-  {
-    super('Psych Event');
-  }
-
-  override function handleEvent(data:SongEventData)
-  {
-    // Does nothing if there is no PlayState camera or stage.
-    if (PlayState.instance == null || PlayState.instance.currentStage == null) return;
-
-    var eventName = data.getString('name');
-    var val1 = data.getString('value1');
-    var val2 = data.getString('value2');
-
-    switch (eventName)
+	  public function new()
     {
+        super('Psych Event');
     }
-  }
 
-  override function getTitle()
-  {
-    return "Psych Engine Event";
-  }
+    public override function getTitle()
+    {
+        return "Psych Event";
+    }
 
-  override function getEventSchema()
-  {
-    return [
-      {
-        name: 'name',
-        title: 'Event Name',
-        type: SongEventFieldType.STRING,
-        defaultValue: 'null',
-      },
-      {
-        name: 'value1',
-        title: 'Value 1',
-        type: SongEventFieldType.STRING,
-        defaultValue: '',
-      },
-      {
-        name: 'value2',
-        title: 'Value 2',
-        type: SongEventFieldType.STRING,
-        defaultValue: ''
-      }
-    ];
-  }
+    override function getEventSchema()
+    {
+        return [
+        {
+            name: 'name',
+            title: 'Event Name',
+            type: SongEventFieldType.STRING,
+            defaultValue: 'null',
+        },
+        {
+            name: 'value1',
+            title: 'Value 1',
+            type: SongEventFieldType.STRING,
+            defaultValue: '',
+        },
+        {
+            name: 'value2',
+            title: 'Value 2',
+            type: SongEventFieldType.STRING,
+            defaultValue: ''
+        }];
+    }
 }
